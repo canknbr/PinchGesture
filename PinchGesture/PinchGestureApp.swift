ad8a1a303f8af2262a63271d175f5268589e1d83
canknbr <cankanbur1@gmail.com>
2023-05-29 11:03:00 +0300

    Initial Commit

---
+++ PinchGesture/PinchGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchGestureApp.swift
+//  PinchGesture
+//
+//  Created by Can Kanbur on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
